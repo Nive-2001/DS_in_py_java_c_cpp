95b4e2f25914182ab08f3e0bc20f38271a6ceb06
Gokul R <srigokulrajendiran@gmail.com>
2021-08-11 13:32:16 +0530

    c++ insert

--- List/Singly Linked List/cpp/Singly Linked List.cpp
+++ List/Singly Linked List/cpp/Singly Linked List.cpp
@@ -1,12 +1,59 @@
 #include <iostream>
-class sll{
-    public:
-        int x;
-}
-
 using namespace std;
 
+struct Node { 
+  int data; 
+  Node *next; 
+};
+
+
+class sll {
+    Node* head;
+    public:
+        sll(){
+            head=NULL;
+        }
+        Node* node(int data){
+            Node* new_node=new Node;
+            new_node->data=data;
+            new_node->next=NULL;
+        }
+        void insert(int data,bool end=false){
+            if(end){
+                if(this->head==NULL){
+                    this->head=node(data);
+                }
+                else{
+                    Node* temp=this->head;
+                    while(temp->next){
+                        temp=temp->next;
+                    }
+                    temp->next=node(data);
+                }
+            }
+            else{
+                Node* temp=node(data);
+                temp->next=this->head;
+                this->head=temp;
+            }
+        }
+        void delete(int data,bool end=false,all=false){
+            
+        }
+        void display(){
+            Node* temp=this->head;
+            while(temp){
+                cout<<temp->data<<"- ";
+                temp=temp->next;
+            }
+            cout<<endl;
+        }
+};
+
 int main() {
-	// your code goes here
+	sll head;
+    head.insert(5);
+    head.insert(6,false);
+    head.display();
 	return 0;
 }
